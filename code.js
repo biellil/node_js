553721409803eb5595773e6cf5406ae1cfd37042
biel.lil <78098857+biellil@users.noreply.github.com>
2023-01-24 18:08:06 -0300

    24/01

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 const express = require('express');
-let routesIndex = require('./routes/code');
+let routesCode = require('./routes/code');
 let routesUsers = require('./routes/user');
 
 let app = express();
 
-app.use(routesIndex);
+app.use(routesCode);
 app.use('/users', routesUsers);
 
 app.listen(3000, '127.0.0.1', () => {
---
+++ routes/user.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let routes = express.Router();
+
+routes.get('/', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users:[{
+            name: 'biel',
+            email: 'biel1313biel@gmail.com',
+            id: 1
+        }]
+    });
+
+});
+
+routes.get('/admin', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: []
+    });
+
+});
+
+module.exports = routes;
