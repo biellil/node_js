d4a6ffff62030376639f6fad67ac745cd37919a6
biel.lil <78098857+biellil@users.noreply.github.com>
2023-01-26 20:46:20 -0300

    26/01

--- routes/user.js
+++ routes/user.js
@@ -1,101 +1,95 @@
 let NeDB = require('nedb');
 let db = new NeDB({
-    filename: 'users.db',
-    autoload : true,
+    filename:'users.db',
+    autoload:true
 });
+
 module.exports = app => {
+
     let route = app.route('/users');
 
     route.get((req, res) => {
-      db.find({}).sort({name:1}).exec((err,users)=>{
 
-          if(err){
-            app.utils.error.send(err,req,res);
-            
-          }
-         else{
-            res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users
+        db.find({}).sort({name:1}).exec((err, users)=>{
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+
+            }
+
         });
 
-         }
-    
     });
-      });
-        
-    
+
     route.post((req, res) => {
-
-        req.assert('name','O nome é obrigatorio.').notEmpty();
-        req.assert('name','O e-mail esta inválido.').notEmpty().isEmail();
-
-        let erros = req.validationErrors();
-        if(errors){
-
-            app.utils.error.send(erros,req,res);
-            return false;
-
-        };
         
-        db.insert(req.body,(err,user)=>{
+        if (!app.utils.validator.user(app, req, res)) return false;
+        
+        db.insert(req.body, (err, user)=>{
 
-            if(err){
-                app.utils.error.send(err,req,res);
-                
-            }
-              else{
-                    res.status(200).json(user)
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+
+                res.status(200).json(user);
+
             }
 
-        })
-    
+        });
+
     });
-    let routeai = app.route('/users/:id');
 
-    routeai.get((req,res)=>{
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
 
         db.findOne({_id:req.params.id}).exec((err, user)=>{
-            if(err){
-                app.utils.error.send(err,req,res);
-                
-            }
-              else{
-                    res.status(200).json(user)
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
             }
 
-        
         });
+
+    });
+
+    routeId.put((req, res) => {
+        
+        if (!app.utils.validator.user(app, req, res)) return false;
+
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+
+        });
+
     });
     
-    routeai.put((req,res)=>{
+    routeId.delete((req, res)=>{
 
-        db.update({_id:req.params.id},req.body,err =>{
-            if(err){
-                app.utils.error.send(err,req,res);
-                
-            }
-              else{
-                    res.status(200).json(Object.assign( req.body, req.params))
+        db.remove({ _id: req.params.id }, {}, err=>{
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(req.params);
             }
 
-        
         });
+
     });
 
-    routeai.delete((req,res)=>{
-
-        db.remove({_id:req.params.id},{},err =>{
-            if(err){
-                app.utils.error.send(err,req,res);
-                
-            }
-              else{
-                    res.status(200).json(req.params);
-            }
-
-        
-        });
-    });
 };
