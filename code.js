4542747efcd53cb6d5431b397d5e89df05d26398
biel.lil <78098857+biellil@users.noreply.github.com>
2023-01-26 20:44:56 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expresssValidator());
 
 consign().include('routes').include('utils').into(app);
 
