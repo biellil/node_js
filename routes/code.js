06275dff4d272b9eac9cda9a2b31127fd8a2e988
biel.lil <78098857+biellil@users.noreply.github.com>
2023-01-27 16:26:52 -0300

    27/01

--- routes/code.js
+++ routes/code.js
@@ -1,11 +1,11 @@
-module.exports = (app) => {
-
+module.exports = app => {
+ 
     app.get('/', (req, res) => {
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Olá</h1>');
+        res.end('<h1>RESTful API</h1><p>Projeto desenvolvido no Curso Completo de JavaScript</p>');
 
     });
-
+    
 };
